afe1e6ac24c427ec0698ea4ff0b885e7e62e6ac9
Yongjun Jeong <lucasjeong99@naver.com>
2020-07-20 01:58:11 +0900

    뭔가 이상하지만 일단 됨

--- navermovie.py
+++ navermovie.py
@@ -9,12 +9,23 @@ soup = BeautifulSoup(res, "html.parser")
 movie_table = soup.select("#content > div.article > div:nth-child(1) > div.lst_wrap > ul")[0]
 movie_list = movie_table.select("dl.lst_dsc")
 
-
 for movie in movie_list:
     movie_namelink = movie.select("dt.tit a")[0]
+
     movie_name = movie_namelink.string
     movie_link = "https://movie.naver.com" + movie_namelink["href"]
+
     movie_star = movie.select("span.num")[0].string
+
     if float(movie_star) >= 9.0:
-        print(f"제목 - {movie_name}")
-        print(f"영화 정보 : {movie_link}")
+        if movie.select("span.link_txt a")[0].string == "드라마":
+            print(f"제목 - {movie_name}")
+            print(f"영화 정보 : {movie_link}")
+        else:
+            if movie.select("span.link_txt a")[1].string == "드라마":
+                print(f"제목 - {movie_name}")
+                print(f"영화 정보 : {movie_link}")
+            else:
+                if movie.select("span.link_txt a")[2].string == "드라마":
+                    print(f"제목 - {movie_name}")
+                    print(f"영화 정보 : {movie_link}")
