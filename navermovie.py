1d7a1fe1dcfe7737054c6b4e6f5a0218acd70ca4
Yongjun Jeong <lucasjeong99@naver.com>
2020-07-20 00:34:04 +0900

    아직 장르 안됨

---
+++ navermovie.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import urllib.request as req
+
+url = "https://movie.naver.com/movie/running/current.nhn"
+res = req.urlopen(url)
+
+soup = BeautifulSoup(res, "html.parser")
+
+movie_table = soup.select("#content > div.article > div:nth-child(1) > div.lst_wrap > ul")[0]
+movie_list = movie_table.select("dl.lst_dsc")
+
+
+for movie in movie_list:
+    movie_namelink = movie.select("dt.tit a")[0]
+    movie_name = movie_namelink.string
+    movie_link = "https://movie.naver.com" + movie_namelink["href"]
+    movie_star = movie.select("span.num")[0].string
+    if float(movie_star) >= 9.0:
+        print(f"제목 - {movie_name}")
+        print(f"영화 정보 : {movie_link}")
